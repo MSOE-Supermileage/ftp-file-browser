146ba28b23fc1cd11210572a3657f548420cc076
Matthew Mahnke <mahnkem@msoe.edu>
2016-04-16 23:04:13 -0500

    Some layout GUIness

--- app/src/main/java/edu/msoe/smv/logfiledownloader/MainActivity.java
+++ app/src/main/java/edu/msoe/smv/logfiledownloader/MainActivity.java
@@ -1,13 +1,84 @@
 package edu.msoe.smv.logfiledownloader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends ListActivity {
+    private List<String> item;
+    private List<String> path;
+    private String root;
+    private TextView myPath;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myPath = (TextView) findViewById(R.id.path);
+
+        root = Environment.getExternalStorageDirectory().getPath();
+
+        getDir("/");
     }
+
+    private void getDir(String dirPath) {
+        myPath.setText("Location: " + dirPath);
+        item = new ArrayList<>();
+        path = new ArrayList<>();
+        File f = new File(dirPath);
+        File[] files = f.listFiles();
+
+        if (!dirPath.equals(root)) {
+            item.add(root);
+            path.add(root);
+            item.add("../");
+            path.add(f.getParent());
+        }
+
+        for (File file : files) {
+            if (!file.isHidden() && file.canRead()) {
+                path.add(file.getPath());
+                if (file.isDirectory()) {
+                    item.add(file.getName() + "/");
+                } else {
+                    item.add(file.getName());
+                }
+            }
+        }
+
+        ArrayAdapter<String> fileList = new ArrayAdapter<>(this, R.layout.row, item);
+        setListAdapter(fileList);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        File file = new File(path.get(position));
+
+        if (file.isDirectory()) {
+            if (file.canRead()) {
+                getDir(path.get(position));
+            } else {
+                new AlertDialog.Builder(this)
+//                        .setIcon(R.drawable.ic_launcher)
+                        .setTitle("[" + file.getName() + "] folder can't be read!")
+                        .setPositiveButton("OK", null).show();
+            }
+        } else {
+            new AlertDialog.Builder(this)
+//                    .setIcon(R.drawable.ic_launcher)
+                    .setTitle("[" + file.getName() + "]")
+                    .setPositiveButton("OK", null).show();
+
+        }
+    }
+
 }
